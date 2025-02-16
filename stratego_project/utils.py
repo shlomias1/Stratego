616458da20faf5fe6ce3a5a1d6dd80c00383bb3c
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-02-16 10:16:52 +0200

    Delete stratego_project directory

--- stratego_project/utils.py
+++
@@ -1,9 +0,0 @@
-def get_display_piece(piece, row_idx, col_idx, water, turn):
-    """Display each piece or water tile."""
-    if (row_idx, col_idx) in water:
-        return "WAT"
-    if piece == "EMPTY":
-        return "---"
-    if ("red" in piece and turn == "blue") or ("blue" in piece and turn == "red"):
-        return "RED" if turn == "blue" else "BLUE"
-    return piece.split("_")[0][:3].upper()
