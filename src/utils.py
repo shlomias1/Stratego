a37303ed743f5e5d890f49512f685b0ab87fbfe5
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-02-24 11:19:58 +0200

    fix bug on _get_display_piece function
    There was a problem with the parameters.

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 import torch
 import numpy as np
+import os
+from config import LOG_DIR
 
 def _get_display_piece(piece, row_idx, col_idx, water, turn):
     """Display each piece or water tile."""
@@ -13,8 +15,10 @@ def _get_display_piece(piece, row_idx, col_idx, water, turn):
     return piece.split("_")[0][:3].upper()
 
 def _create_log(log_msg, log_type, log_file = "logs.txt"):
+    os.makedirs(LOG_DIR, exist_ok=True)
+    log_path = os.path.join(LOG_DIR, log_file)
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    with open(log_file, "a") as log:
+    with open(log_path, "a") as log:
         log.write(f'{log_type} : {log_msg} | {current_time} \n')
 
 def Connect_CUDA():
@@ -22,16 +26,10 @@ def Connect_CUDA():
     if torch.cuda.is_available():
         device = torch.device("cuda")
         gpu_name = torch.cuda.get_device_name(0)
-        gpu_memory = torch.cuda.get_device_properties(0).total_memory / 1e9  # גודל זיכרון ב-GB
-        log_msg1 = f"✅ Running on GPU: {gpu_name} | Memory: {gpu_memory:.2f} GB"
-        log_msg2 = f"🔍 GPU Allocated Memory: {torch.cuda.memory_allocated() / 1e9:.2f} GB"
-        log_msg3 = f"🔍 GPU Cached Memory: {torch.cuda.memory_reserved() / 1e9:.2f} GB"
-        _create_log(log_msg1, log_type)
-        _create_log(log_msg2, log_type)
-        _create_log(log_msg3, log_type)
-        print(log_msg1)
-        print(log_msg2)
-        print(log_msg3)
+        gpu_memory = torch.cuda.get_device_properties(0).total_memory / 1e9 
+        log_msg = f"✅ Running on GPU: {gpu_name} | Memory: {gpu_memory:.2f} GB \n GPU Allocated Memory: {torch.cuda.memory_allocated() / 1e9:.2f} GB \n GPU Cached Memory: {torch.cuda.memory_reserved() / 1e9:.2f} GB"
+        _create_log(log_msg, log_type)
+        print(log_msg)
     else:
         device = torch.device("cpu")
         log_msg = "Running on CPU - No GPU detected!"
